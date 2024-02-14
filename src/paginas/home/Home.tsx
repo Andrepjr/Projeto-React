e734308c148653c871a5daa8f6737273470877bd
André Prado Junior <116444319+Andrepjr@users.noreply.github.com>
2024-02-14 12:00:48 -0300

    Primeiro Commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css"
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Manipulando o Mundo</h1>
+            <img className="img" src="https://pbs.twimg.com/media/FPvaLaTX0A0KHsB.jpg" alt="Imagem Musashi" />
+        </>
+    )
+}
+
+export default Home;
